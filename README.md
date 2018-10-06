7c781bb19683710eee8aa85764c99f02d0c8ed00
Proteas <proteas.wang@gmail.com>
2018-10-06 20:30:52 +0800

    libdispatch-913.60.2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # install firehose library
-install firehose(x86_64, arm64) for building xnu-4570.41.2
+install firehose(x86_64, arm64) for building xnu-4570.71.2
 
 check out the code, and run install.sh:
 
@@ -7,4 +7,4 @@ check out the code, and run install.sh:
 ./install-firehose.sh
 ```
 
-then build xnu-4570.41.2 with Xcode-v9.2.
+then build xnu-4570.71.2 with Xcode-v9.4.1.
--- install-firehose.sh
+++ install-firehose.sh
@@ -6,7 +6,8 @@ CURRENT_DIR=`pwd`
 popd > /dev/null
 # ================================================================ #
 
-SDK_ROOT=`xcodebuild -version -sdk macosx Path`
+# SDK_ROOT=`xcodebuild -version -sdk macosx Path`
+SDK_ROOT=`xcodebuild -version -sdk iphoneos Path`
 echo "Target SDK Path: ${SDK_ROOT}"
 # ================================================================ #
 
@@ -16,21 +17,25 @@ echo "Target SDK Path: ${SDK_ROOT}"
 # LIBDISPATCH_DIR_NAME="libdispatch-703.50.37"
 # LIBDISPATCH_DIR_NAME="libdispatch-913.1.6"
 # LIBDISPATCH_DIR_NAME="libdispatch-913.20.5"
-LIBDISPATCH_DIR_NAME="libdispatch-913.30.4"
+# LIBDISPATCH_DIR_NAME="libdispatch-913.30.4"
+LIBDISPATCH_DIR_NAME="libdispatch-913.60.2"
 
 echo "Install: ${LIBDISPATCH_DIR_NAME}"
 # ================================================================ #
 
-TARGET_HEADER_DIR="${SDK_ROOT}/usr/local/include/kernel/os"
+TARGET_HEADER_DIR="${SDK_ROOT}/usr/local/include/firehose"
 TARGET_LIB_DIR="${SDK_ROOT}/usr/local/lib/kernel"
+
+echo "Dest Header Dir: ${TARGET_HEADER_DIR}"
+echo "Dest Library Dir: ${TARGET_LIB_DIR}"
 # ================================================================ #
 
 mkdir -p "${TARGET_HEADER_DIR}"
 mkdir -p "${TARGET_LIB_DIR}"
 # ================================================================ #
 
-cp -rf "${CURRENT_DIR}/${LIBDISPATCH_DIR_NAME}/firehose_buffer_private.h" "${TARGET_HEADER_DIR}/"
-cp -rf "${CURRENT_DIR}/${LIBDISPATCH_DIR_NAME}/libfirehose_kernel.a" "${TARGET_LIB_DIR}/"
+ditto "${CURRENT_DIR}/${LIBDISPATCH_DIR_NAME}/firehose" "${TARGET_HEADER_DIR}"
+cp -rf "${CURRENT_DIR}/${LIBDISPATCH_DIR_NAME}"/libfirehose_kernel*.a "${TARGET_LIB_DIR}/"
 # ================================================================ #
 
 echo "======== Done ========"
